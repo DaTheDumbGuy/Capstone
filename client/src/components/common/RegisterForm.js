d36cab491de71db76e24c8fd99bf9b833e104d99
DessaCamara <dessamarie.cmr@gmail.com>
2023-05-08 01:11:13 +0800

    test

--- client/src/components/common/RegisterForm.js
+++ client/src/components/common/RegisterForm.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import '../../styles/common/Register.css';
 
 function RegisterForm({ onSubmit, serverError }) {
     const [firstName, setFirstName] = useState('');
